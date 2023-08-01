659db0188fd76a4dea43b84bc4c6b7393de2b69e
Victor Franco <60646163+victorfrancoramos@users.noreply.github.com>
2023-08-01 18:17:59 +0200

    Update create_volume.py

--- create_volume.py
+++ create_volume.py
@@ -1,1 +1,25 @@
-import requestsimport jsonurl = "https://cluster1.demo.netapp.com/api/storage/volumes?return_timeout=0&return_records=false"payload = json.dumps({  "aggregates": [    {      "name": "cluster1_01_SSD_1"    }  ],  "name": "{{vol_name}}",  "size": "200mb",  "svm": {    "name": "svm1_cluster1"  }})headers = {  'Content-Type': 'application/json',  'Authorization': 'Basic YWRtaW46TmV0YXBwMSE='}response = requests.request("POST", url, headers=headers, data=payload)print(response.text)
+vserver-get import requests
+import json
+
+url = "https://cluster1.demo.netapp.com/api/storage/volumes?return_timeout=0&return_records=false"
+
+payload = json.dumps({
+  "aggregates": [
+    {
+      "name": "cluster1_01_SSD_1"
+    }
+  ],
+  "name": "{{vol_name}}",
+  "size": "200mb",
+  "svm": {
+    "name": "svm1_cluster1"
+  }
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic YWRtaW46TmV0YXBwMSE='
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
